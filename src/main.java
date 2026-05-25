3882f2e2d835066069c2d6b3a1d8a2b35ff62c1a
Rajeev-Cryptographer <Rajeevofficial3@gmailcom>
2026-05-25 23:05:05 +0530

    Pushing java-code-run workflow

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Actions!");
+        System.out.println("Java CI practice repo");
+    }
+}
